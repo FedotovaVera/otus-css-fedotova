10c724d9e572ca95e26fa47389c72fe367799ca2
Vera <111522773+FedotovaVera@users.noreply.github.com>
2023-07-17 23:04:57 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   devtool: 'source-map',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
     clean: true,
   },
 
